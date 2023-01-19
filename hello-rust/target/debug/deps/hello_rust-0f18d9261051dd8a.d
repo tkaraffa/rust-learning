44e4275ee9194daa7b9d3a37bea201d85eafc709
Tom Karaffa <tom.karaffa@protonmail.com>
2023-01-18 16:44:08 -0800

    add hello

---
+++ hello-rust/target/debug/deps/hello_rust-0f18d9261051dd8a.d
@@ -0,0 +1,5 @@
+/home/tom/repos/rust-learning/hello-rust/target/debug/deps/hello_rust-0f18d9261051dd8a: src/main.rs
+
+/home/tom/repos/rust-learning/hello-rust/target/debug/deps/hello_rust-0f18d9261051dd8a.d: src/main.rs
+
+src/main.rs:
---
+++ hello-rust/target/debug/hello-rust.d
@@ -0,0 +1,1 @@
+/home/tom/repos/rust-learning/hello-rust/target/debug/hello-rust: /home/tom/repos/rust-learning/hello-rust/src/main.rs
