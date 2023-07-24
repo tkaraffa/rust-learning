2017afd8921cfe50768fbe2a56a51fb87019f37e
Tom Karaffa <tom.karaffa@protonmail.com>
2023-07-24 13:54:49 -0700

    .

--- process/src/main.rs
+++ process/src/main.rs
@@ -2,30 +2,36 @@ mod schema;
 use clap::Parser;
 use std::fs;
 
-use schema::{get_schema_by_version, read_data};
+use schema::{get_schema_by_version, CURRENT_VERSION};
 
-// #[derive(Parser, Debug)]
-// #[command(author, version, about, long_about = None)]
-// struct Args {
-//     /// Path in which to process files
-//     in: String,
-// }
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Directory from which to process files
+    input_directory: String,
+
+    /// Schema version to use for processing data
+    #[arg(short, long, default_value_t = CURRENT_VERSION)]
+    schema_version: usize,
+}
 
 fn main() {
-    // let args = Args::parse();
-    // let input_directory = args.in;
-    let input_directory = "test";
-    let version: usize = 1;
+    let args = Args::parse();
+    let input_directory = args.input_directory;
+    let schema_version: usize = args.schema_version;
 
     let input_directory = fs::read_dir(input_directory).unwrap();
     for path in input_directory {
-        let data = read_data(path.unwrap().path());
-        let schema = get_schema_by_version(data, version);
+        let data = fs::read_to_string(path.unwrap().path()).unwrap();
+        let schema = get_schema_by_version(data.clone(), schema_version);
+
         println!("{}", schema.get_data());
     }
 }
-
 /*
+ THE SCHEMA TRAIT SHOULD BE A TYPE
+ and there should be a ..."process" trait that uses that type
+
 so, main idea is
 * have some number of schemas that dictate how the JSON data should look
     * these might be parsed from YAML (into structs) or be structs themselves
