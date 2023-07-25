9ed2ea73bec153fe14522ccd0e8439cd2eead711
Tom Karaffa <tom.karaffa@protonmail.com>
2023-07-25 08:08:34 -0700

    this is how you blanketedly handle errors

--- process/src/schema.rs
+++ process/src/schema.rs
@@ -2,7 +2,7 @@ use core::fmt::Debug;
 use serde::{Deserialize, Serialize};
 use serde_json;
 use std::io;
-
+use std::io::{Error, ErrorKind};
 // use this as the default value
 // alternatively, make it go through env vars
 // better yet, go through CLI, then env, then here
@@ -16,7 +16,12 @@ pub fn get_schema_by_version(data: String, version: usize) -> Result<serde_json:
     match version {
         2 => SchemaV2::from_data(data),
         1 => SchemaV1::from_data(data),
-        _ => Ok(DefaultSchema::from_data(data)).expect_err("Nooo"),
+        _ => {
+            return Err(Error::new(
+                ErrorKind::Other,
+                format!("Error SchemaV{version}"),
+            ))
+        }
     }
 }
 
@@ -51,20 +56,6 @@ pub struct SchemaV1 {
 }
 
 impl Schema for SchemaV1 {
-    fn from_data(data: String) -> Result<serde_json::Value, io::Error> {
-        let schema: Self = serde_json::from_str(&data)?;
-        Ok(serde_json::json!(schema))
-    }
-    fn get_data(&self) -> Result<String, io::Error> {
-        let data: String = serde_json::to_string(&self)?;
-        Ok(data)
-    }
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct DefaultSchema {}
-
-impl Schema for DefaultSchema {
     fn from_data(data: String) -> Result<serde_json::Value, io::Error> {
         let schema: Self = serde_json::from_str(&data)?;
         Ok(serde_json::json!(schema))
